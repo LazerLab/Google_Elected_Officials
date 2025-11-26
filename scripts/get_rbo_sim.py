d411b770d06edb6eadc2e454f3cc1d159ba8adb5
Aywan-1 <44881001+Aywan-1@users.noreply.github.com>
2025-11-26 08:43:17 -0800

    Update get_rbo_sim.py

--- scripts/get_rbo_sim.py
+++ scripts/get_rbo_sim.py
@@ -3,7 +3,6 @@ Take input from house_parquet_no_error, calculate similarity for search results.
 
 Input: parquet version of results
 Output: csv of similarity
-Based on: /home/guozhen1/google_audit_reproduce/workflow/old/result_similarity/get_loc_rbo_sim.py 
 '''
 
 import sys
@@ -52,7 +51,6 @@ def sample_different_date(row, date_pool):
     
 def get_results_by_date(rbo_sample_df, date):
     df = get_new_day_df(date)
-    #df = pd.merge(df, house_domain_clean, on='domain', how='left')
     
     date_samples = rbo_sample_df[rbo_sample_df["date_file"] == date]
     date_samples = date_samples.loc[:, ~date_samples.columns.str.startswith('old_date')]
@@ -92,7 +90,6 @@ def get_results_by_date(rbo_sample_df, date):
     df_pairs = df_pairs.dropna(subset=['url_list1', 'url_list2'])
 
     df_pairs_copy = df_pairs.copy()
-    #print(df_pairs_copy.head())
     if len(df_pairs_copy) > 0:
 
         df_pairs_copy['rbo'] = df_pairs_copy.apply(
@@ -103,14 +100,13 @@ def get_results_by_date(rbo_sample_df, date):
     merged = date_samples.merge(df_pairs_copy[['qry', 'loc_1', 'loc_2']], 
                        on=['qry','loc_1', 'loc_2'], how='left', indicator=True)
     
-            # Filter where the pair was not found in df2
+     # Filter where the pair was not found in df2
     resample_df = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
 
     return df_pairs_copy, resample_df
 
 
 def get_results_df(rbo_sample_df, result_file_path):
-    #house_domain_clean = house_domain[['domain', 'news']]
 
     date_list = rbo_sample_df['date_file'].drop_duplicates().to_list()
 
@@ -123,12 +119,7 @@ def get_results_df(rbo_sample_df, result_file_path):
         
         resample.append(resample_df)
         result.append(result_df)
- 
-        #if len(result_df)>0:
-            #if os.path.exists(result_file_path):
-               # old_df = pd.read_parquet(result_file_path)
-                #result_df =  pd.concat([old_df, result_df], axis=0, ignore_index=True)
-            #result_df.to_parquet(result_file_path)
+
         i = i+1
     
     resample_df_big = pd.concat(resample, ignore_index=True)
@@ -145,25 +136,12 @@ if __name__ == "__main__":
 
     DATA_ROOT = "/net/lazer/lab-lazer/shared_projects/google_audit_reproduce/intermedidate_files"
     RAW_PARQUET_PATH = os.path.join(DATA_ROOT, "summary_by_day")
-
-    #house_domain = pd.read_csv("data/house_domain_final_122024.csv")
-    rbo_sample_df = pd.read_parquet("data/rbo_sim_sampling.parquet")
-    #rbo_sample_done = pd.read_parquet("data/rbo_result.parquet")
-
-    #merged = rbo_sample_df_orig.merge(rbo_sample_done[['qry', 'loc_1', 'loc_2', 'date_file']], 
-                      #on=['qry','loc_1', 'loc_2', 'date_file'], how='left', indicator=True)
     
-            # Filter where the pair was not found in df2
-    #resample_df = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
-    #resample_df = pd.read_parquet("data/resample_df.parquet")
-    #print(len(resample_df))
+    rbo_sample_df = pd.read_parquet("data/rbo_sim_sampling.parquet")
 
     all_dates = rbo_sample_df['date_file'].drop_duplicates().to_list()
-
-    #all_dates = rbo_sample_df_orig['date_file'].drop_duplicates().to_list()
-
     
-    resample_df = get_results_df(rbo_sample_df, #date_list,#house_domain,
+    resample_df = get_results_df(rbo_sample_df,
                                        "data/rbo_result.parquet")
 
     i = 0
